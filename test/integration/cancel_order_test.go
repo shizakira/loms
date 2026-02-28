a30927d54018ec74415efa7d0b3bb2966f52ccdd
shizakira <ikirachn@gmail.com>
2026-02-28 21:26:07 +0500

    add integration test

---
+++ test/integration/cancel_order_test.go
@@ -0,0 +1,55 @@
+//go:build integration
+
+package test
+
+import (
+	"context"
+
+	pb "github.com/shizakira/loms/gen/grpc/loms_v1"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (s *Suite) Test_OrderCancel_Success() {
+	created, err := s.OrderCreate(context.Background(), &pb.OrderCreateRequest{
+		User: 1,
+		Items: []*pb.Item{
+			{Sku: 1076963, Count: 1},
+		},
+	})
+	s.NoError(err)
+
+	_, err = s.OrderCancel(context.Background(), &pb.OrderCancelRequest{
+		OrderId: created.GetOrderId(),
+	})
+	s.NoError(err)
+
+	info, err := s.OrderInfo(context.Background(), &pb.OrderInfoRequest{
+		OrderId: created.GetOrderId(),
+	})
+	s.NoError(err)
+	s.Equal("cancelled", info.GetStatus())
+}
+
+func (s *Suite) Test_OrderCancel_NotFound() {
+	_, err := s.OrderCancel(context.Background(), &pb.OrderCancelRequest{
+		OrderId: 99999999,
+	})
+	s.Equal(codes.NotFound, status.Code(err))
+}
+
+func (s *Suite) Test_OrderCancel_InvalidStatus() {
+	created, err := s.OrderCreate(context.Background(), &pb.OrderCreateRequest{
+		User: 1,
+		Items: []*pb.Item{
+			{Sku: 1076963, Count: 1},
+		},
+	})
+	s.NoError(err)
+
+	_, err = s.OrderPay(context.Background(), &pb.OrderPayRequest{OrderId: created.GetOrderId()})
+	s.NoError(err)
+
+	_, err = s.OrderCancel(context.Background(), &pb.OrderCancelRequest{OrderId: created.GetOrderId()})
+	s.Equal(codes.FailedPrecondition, status.Code(err))
+}
