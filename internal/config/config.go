0a24becbd481396d884a0768250be67c26fb342c
vi <ikiracn@gmail.com>
2026-02-28 21:21:14 +0500

    init project structure

---
+++ internal/config/config.go
@@ -0,0 +1,47 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/joho/godotenv"
+	"github.com/kelseyhightower/envconfig"
+	"github.com/shizakira/loms/internal/controller/http_gateway"
+	"github.com/shizakira/loms/pkg/logger"
+
+	"github.com/shizakira/loms/internal/controller/grpc"
+	"github.com/shizakira/loms/pkg/postgres"
+)
+
+type App struct {
+	Name    string `envconfig:"APP_NAME"    required:"true"`
+	Version string `envconfig:"APP_VERSION" required:"true"`
+}
+type Config struct {
+	App      App
+	Postgres postgres.Config
+	Logger   logger.Config
+	GRPC     grpc.Config
+	HTTP     http_gateway.Config
+}
+
+func New() (Config, error) {
+	return load(".env")
+}
+
+func NewTest() (Config, error) {
+	return load(".env.test")
+}
+
+func load(env string) (Config, error) {
+	var config Config
+
+	if err := godotenv.Load(env); err != nil {
+		return config, fmt.Errorf("godotenv.Load: %w", err)
+	}
+
+	if err := envconfig.Process("", &config); err != nil {
+		return config, fmt.Errorf("envconfig.Process: %w", err)
+	}
+
+	return config, nil
+}
